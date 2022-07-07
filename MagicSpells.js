a409c42eb007cac259ffb42df86d4cb005f67c1b
Zeus <zeusofolympus1000@gmail.com>
2022-07-07 17:43:18 -0400

    anything scientific

--- MagicSpells.js
+++ MagicSpells.js
@@ -3,4 +3,5 @@ const ANYTHING_OVER_100_PCT_EX = "/ (?![1-9][0-9]%)(?!100%)((\b[1][0-9][1-9]%(\b
 const ANYTHING_UNDER_0_IN = "/ \b0\.[0-9]*\b /g";
 const ANYTHING_UNDER_0_EX = "/ (?!\b0.[0][0]*\b)(\b0\.[0-9]*\b) /g";
 const ANYTHING_UNDER_0_EX_TRUNC = "\b(0\.[0-9]*)(?!0*\b)[0-9]"
+const ANYTHING_SCIENTIFIC="\b((0\.[0-9]*)|([1-9]*\.[0-9]*))(?!0*\b)[0-9]e(\+|-|)[1-9][0-9]*"
 exports = {BETWEEN_1_PCT_AND_100_PCT}
