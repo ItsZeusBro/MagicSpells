19ddeda68d480786f84f1837991a422b5b85c8bc
Zeus <zeusofolympus1000@gmail.com>
2022-07-07 17:46:47 -0400

    cleaning up a bit

--- MagicSpells.js
+++ MagicSpells.js
@@ -2,6 +2,13 @@ const BETWEEN_1_PCT_AND_100_PCT = "/ (?!\b[1][0-9][1-9]%)(?!(\b[2-9][0-9][0-9][0
 const ANYTHING_OVER_100_PCT_EX = "/ (?![1-9][0-9]%)(?!100%)((\b[1][0-9][1-9]%(\b))|(\b[1-9][0-9][0-9]*%)) /g";
 const ANYTHING_UNDER_0_IN = "/ \b0\.[0-9]*\b /g";
 const ANYTHING_UNDER_0_EX = "/ (?!\b0.[0][0]*\b)(\b0\.[0-9]*\b) /g";
-const ANYTHING_UNDER_0_EX_TRUNC = "\b(0\.[0-9]*)(?!0*\b)[0-9]"
-const ANYTHING_SCIENTIFIC="\b((0\.[0-9]*)|([1-9]*\.[0-9]*))(?!0*\b)[0-9]e(\+|-|)[1-9][0-9]*"
-exports = {BETWEEN_1_PCT_AND_100_PCT}
+const ANYTHING_UNDER_0_EX_TRUNC = "\b(0\.[0-9]*)(?!0*\b)[0-9]\b"
+const ANYTHING_SCIENTIFIC="\b((0\.[0-9]*)|([1-9]*\.[0-9]*))(?!0*\b)[0-9]e(\+|-|)[1-9][0-9]*\b"
+exports = {
+    BETWEEN_1_PCT_AND_100_PCT, 
+    ANYTHING_OVER_100_PCT_EX, 
+    ANYTHING_UNDER_0_IN, 
+    ANYTHING_UNDER_0_EX, 
+    ANYTHING_UNDER_0_EX_TRUNC,
+    ANYTHING_SCIENTIFIC
+}
