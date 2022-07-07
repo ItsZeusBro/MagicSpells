046284ef70abf18083ce2ee474de6487276ae4f0
Zeus <zeusofolympus1000@gmail.com>
2022-07-07 17:25:02 -0400

    adding global

--- MagicSpells.js
+++ MagicSpells.js
@@ -1,5 +1,5 @@
 const BETWEEN_1_PCT_AND_100_PCT = "/ (?!\b[1][0-9][1-9]%)(?!(\b[2-9][0-9][0-9][0-9]*)%)\b[1-9][0-9]?[0-9]?% /g"
 const ANYTHING_OVER_100_PCT_EX = "/ (?![1-9][0-9]%)(?!100%)((\b[1][0-9][1-9]%(\b))|(\b[1-9][0-9][0-9]*%)) /g"
 const ANYTHING_UNDER_0_IN = "/ \b0\.[0-9]*\b /g"
-const ANYTHING_UNDER_0_EX = "(?!\b0.[0][0]*\b)(\b0\.[0-9]*\b)"
+const ANYTHING_UNDER_0_EX = "/ (?!\b0.[0][0]*\b)(\b0\.[0-9]*\b) /g"
 exports = {BETWEEN_1_PCT_AND_100_PCT}
