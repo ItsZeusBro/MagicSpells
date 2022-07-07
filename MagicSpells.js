73542f97855a2029b932cb755b496ea8903e303c
Zeus <zeusofolympus1000@gmail.com>
2022-07-07 17:06:19 -0400

    anything under 0 and nothing else (inclusive)

--- MagicSpells.js
+++ MagicSpells.js
@@ -1,3 +1,4 @@
 const BETWEEN_1_PCT_AND_100_PCT = "(?!\b[1][0-9][1-9]%)(?!(\b[2-9][0-9][0-9][0-9]*)%)\b[1-9][0-9]?[0-9]?%"
 const ANYTHING_OVER_100_PCT = "(?![1-9][0-9]%)(?!100%)((\b[1][0-9][1-9]%(\b))|(\b[1-9][0-9][0-9]*%))"
+const ANYTHING_UNDER_0 = "\b(0\.[0-9]*[0-9])\b"
 exports = {BETWEEN_1_PCT_AND_100_PCT}
