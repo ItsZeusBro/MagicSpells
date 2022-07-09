e0e0053ba64d5729f43e0873ec53aa6d83afa578
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-09 01:22:38 -0400

    Update MagicSpells.js

--- MagicSpells.js
+++ MagicSpells.js
@@ -3,5 +3,5 @@ export const ANYTHING_OVER_100_PCT_EX = "/ (?![1-9][0-9]%)(?!100%)((\b[1][0-9][1
 export const ANYTHING_UNDER_0_IN = "/ \b0\.[0-9]*\b /g";
 export const ANYTHING_UNDER_0_EX = "/ (?!\b0.[0][0]*\b)(\b0\.[0-9]*\b) /g";
 export const ANYTHING_UNDER_0_EX_TRUNC = "/ \b(0\.[0-9]*)(?!0*\b)[0-9]\b /g"
-export const ANYTHING_SCIENTIFIC="/ \b((0\.[0-9]*)|([1-9]*\.[0-9]*))(?!0*\b)[0-9]e(\+|-|)[1-9][0-9]*\b /g"
+export const ANYTHING_SCIENTIFIC="/ (((0\.[0-9]*)|([1-9]*\.[0-9]*))(?!0*\b)[0-9]E(\+|-|)[1-9][0-9]*) /g"
 export const DEWEY_DECIMAL="/ \b[0-9][0-9][0-9]\.?[0-9]?[0-9]?[0-9]? ([A-Z][a-z]{1,44} ?)([a-z]{1,45} ?)*\b/g"
