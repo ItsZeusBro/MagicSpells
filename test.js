9d84d62fb12c62a89f04f4ddb2ea0c724fe7be5b
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-10 16:00:54 -0400

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-import * as MagicSpells from "./MagicSpells.js"
-console.log(MagicSpells.BETWEEN_1_PCT_AND_100_PCT)
-console.log(MagicSpells.ANYTHING_OVER_100_PCT_EX)
-console.log(MagicSpells.ANYTHING_UNDER_0_IN)
-console.log(MagicSpells.ANYTHING_UNDER_0_EX)
-console.log(MagicSpells.ANYTHING_UNDER_0_EX_TRUNC)
-console.log(MagicSpells.ANYTHING_SCIENTIFIC)
