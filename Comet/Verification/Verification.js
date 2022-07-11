4e5ac116696a8dbdb604e58902083d6fee47d99b
Zeus <zeusofolympus1000@gmail.com>
2022-07-11 18:21:29 -0400

    comet commit strategy

--- Comet/Verification/Verification.js
+++ Comet/Verification/Verification.js
@@ -1,25 +1,26 @@
-export class Verification extends Comet{
+export class Tracker extends Comet{
     constructor(){
     }
 
-    //You can register a file at anytime with comet
-    //the last 1000 merge diffs are kept for the registered file
-    //in the diffs folder.
+    register(path){
+        //register the path (could be a directory or file)
+        //either way the path to the file must be created in the diff folder from
+        //the project root
 
-    //pre-commit hooks are used to call Comet's verification code
-    //to verify the user's changes to a registered file using their
-    //git ssh key file.
-    
-    //diff logger relies on the initial state of the registered file
-    //diffs are secured in a hash chain for sanity.
-    
-    //step 1: take a snapshot of the test case file
-    //step 2: take the hash of every test case
-    //step 3: sort hashes
-    //step 4: hash of sorted hashes
-    //step 5: associate snapshot with hash of sorted hashes
-    //diffs should be stored in diff folder and you should be able
-    //to reverify the hash of sorted hashes from anypoint. 
+        //registry is just the existence of a file in this pattern
+        //registry of a dir is the existence of a meta-file belonging to the dir
+        //directoryname.comet is how we will keep track of dir meta data
+        //all diffs inside a file are kept in the diff file corresponding to
+        //the actual registered file.
 
 
+        //A diff hash chain is created in the dir meta file and dirs
+        //higher in the tree hash these as well. 
+
+        //This is not really intended to replace version control.
+        //its designed to bring sanity to things you want to keep
+        //an eye on. Like test cases. (things that shouldn't be removed unless its wholesale)
+        
+
+    }
 }
