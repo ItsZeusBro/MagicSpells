1e919c540a98e40736f5743d27b36e572a5f7ab3
Zeus <zeusofolympus1000@gmail.com>
2022-07-11 19:20:48 -0400

    registry should not offer everything without specifiying what you want. There is some default behavior when you comet from a file, and there is specific behavior when you invoke the registry functions from comet api

--- Comet/Comet.js
+++ Comet/Comet.js
@@ -2,26 +2,36 @@ import * as fs from "node:fs";
 import { Registry } from "./Source/Registry/Registry.js";
 
 export class Comet{
-   constructor(root){
-        //Comet only needs to be initilized once for an entire project
-        //so long as the project name is provided
-        this.root=root
-        this.Registry = new Registry(this.root)
-        this.index=process.cwd().split(this.root)[0]+this.root+"/Comet/index/";
-        
-   }
+    constructor(root){
+            //Comet only needs to be initilized once for an entire project
+            //so long as the project name is provided
+            this.root=root
+            this.Registry = new Registry(this.root)
+            this.index=process.cwd().split(this.root)[0]+this.root+"/Comet/index/";
+            process.on('uncaughtException', (err, origin) => {
+                var indexP = this.getCaller(err.stack.split('\n'))
+                this._comet(['There was an uncaught error '+ err.stack], indexP)            
+                process.exit(1);
+            });
+            
+    }
 
-   comet(...data){
-    //this should know the absolute path to the file from where comet() its called
-    var abs = this.getCaller(new Error().stack.split('\n'))
-    var indexP=abs.split(this.root)[0]+this.root+"/Comet/index"+abs.split(this.root)[1]
-    console.log(indexP)
-   }
-   getCaller(stack) {
-        return stack[2].slice(
+    comet(...data){
+        //this should know the absolute path to the file from where comet() its called
+        var indexP = this.getCaller(new Error().stack.split('\n'))
+        this._comet(data, indexP)
+    }
+    _comet(data, indexP){
+        //check if indexP exists, if not, create it and register it without diff behavior
+        //we only want to register things according to what functionality is required
+    }
+    getCaller(stack) {
+        var abs = stack[2].slice(
             stack[2].lastIndexOf('(')+1, 
             stack[2].lastIndexOf('.js')+3
         )
+        return abs.split(this.root)[0]+this.root+"/Comet/index"+abs.split(this.root)[1]
+
     }
 }
 
