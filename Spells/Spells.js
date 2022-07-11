4edbe96e363a1cd67b3b3ee1007726550deeaf62
Zeus <zeusofolympus1000@gmail.com>
2022-07-10 23:35:38 -0400

    we only need sentances, not full blown paragraphs for nlp

--- Spells/Spells.js
+++ Spells/Spells.js
@@ -4,3 +4,4 @@ export const IS_FLOAT=/^[0-9]+\.[0-9]+$/
 export const IS_BINARY=/^[0-1]+$/
 export const IS_ALPHA_STRING=/^[a-zA-Z]+$/
 export const IS_ALPHA_NUMERIC_STRING= /^[a-zA-Z0-9]+$/
+export const IS_SENTANCE=/^[A-Z][a-z]*( |, |; )?([a-z]+(')?( |, |; )?)*((\.\.\.)|\?|\!|\.)/
