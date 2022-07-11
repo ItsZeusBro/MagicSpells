30562f552bad896f523c5f1b3926edd4ae389852
Zeus <zeusofolympus1000@gmail.com>
2022-07-11 04:20:03 -0400

    NOT SO FAST!

--- Source/Matchic.js
+++ Source/Matchic.js
@@ -22,21 +22,6 @@ export class Matchic extends Comet{
         }
     }
 
-    no(string, regex){
-        var tokens;
-        try{
-            tokens = string.split(/(\s)/g); //PROBABLY A BUG IN HERE
-        }catch{
-            return true;
-        }
-        tokens.forEach(token => {
-            if(this.is(token, regex)){
-                return false;
-            }
-        });
-        return true;
-    }
-
     has(string, regex){
         var tokens;
         try{
@@ -54,29 +39,22 @@ export class Matchic extends Comet{
 
     isInteger(string){return this.is(string, IS_INTEGER)}
     hasInteger(string){return this.has(string, IS_INTEGER)}
-    noInteger(string){return this.no(string, IS_INTEGER)}
 
     isFloat(string){return this.is(string, IS_FLOAT)}
     hasFloat(string){return this.has(string, IS_FLOAT)}
-    noFloat(string){return this.no(string, IS_FLOAT)}
 
     isBinary(string){return this.is(string, IS_BINARY)}
     hasBinary(string){return this.has(string, IS_BINARY)}
-    noBinary(string){return this.no(string, IS_BINARY)}
 
     isAlpha(string){return this.is(string, IS_ALPHA_STRING)}
     hasAlpha(string){return this.has(string, IS_ALPHA_STRING)}
-    noAlpha(string){return this.no(string, IS_ALPHA_STRING)}
 
     isAlphaNumeric(string){return this.is(string, IS_ALPHA_NUMERIC_STRING)}
     hasAlphaNumeric(string){return this.has(string, IS_ALPHA_NUMERIC_STRING)}
-    noAlphaNumeric(string){return this.no(string, IS_ALPHA_NUMERIC_STRING)}
 
     isScientific(string){return this.is(string, IS_SCIENTIFIC)}
     hasScientific(string){return this.has(string, IS_SCIENTIFIC)}
-    noScientific(string){return this.no(string, IS_SCIENTIFIC)}
 
     isSentance(string){return this.is(string, IS_SENTANCE)}
     hasSentance(string){return this.has(string, IS_SENTANCE)}
-    noSentance(string){return this.no(string, IS_SENTANCE)}    
 }
