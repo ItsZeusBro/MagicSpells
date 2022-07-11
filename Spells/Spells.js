2c4214156452f2b0ba4cc324ddb920ef9e5bb7d6
Zeus <zeusofolympus1000@gmail.com>
2022-07-10 20:25:08 -0400

    meta-scientific notation regex pattern

--- Spells/Spells.js
+++ Spells/Spells.js
@@ -1,5 +1,5 @@
 export const META_NUM_RANGE= /\[[0-9]*\-[0-9]*\]/;
 export const META_CHAR_RANGE=/\[([a-z]|[A-Z])-([a-z]|[A-Z])\]/;
 export const META_E = /e\\\+\?\\\-\?\[[0-9]*\-[0-9]*\]\*/;
+export const META_SCIENTIFIC = /\/\[[0-9]*\-[0-9]*\]\*\\\.\[[0-9]*\-[0-9]*\]\*e\\\+\?\\\-\?\[[0-9]*\-[0-9]*\]\*\//;
 export const SCIENTIFIC=/[0-9]*\.[0-9]*e\+?\-?[0-9]*/;
-
