33a0890e495c873e04b440697b5f17042223d482
Zeus <zeusofolympus1000@gmail.com>
2022-07-11 20:31:14 -0400

    way simpler

--- Comet/Source/Registry/Registry.js
+++ Comet/Source/Registry/Registry.js
@@ -16,13 +16,10 @@
 //its designed to bring sanity to things you want to keep
 //an eye on. Like test cases. (things that shouldn't be removed unless its wholesale)
 
-import {LOG, TRACK} from "./rTypes.js";
 import * as fs from "node:fs";
 
 export class Registration{
-    constructor(path){
-        this.logP = this.create_log(path)
-    }
+
     get_Registration(logP){
 
         //return this
@@ -30,9 +27,7 @@ export class Registration{
     _log(data){
         fs.writeFileSync(this.logP, data.join(' ')+'\n', {flag:'a'})
     }
-    _create_log(path){
 
-    }
     register(indexP, rType){
         //creates registration and
         //return this
@@ -43,30 +38,21 @@ export class Registration{
 }
 
 export class Registry{
-    constructor(){
-    }
     //TODO
-    get_registration(indexP, rType){
-        return new Registration().get_Registration(indexP, rType)
-        
-    }
     exists(indexP, rType){
         return new Registration().exists(indexP, rType)
+    }
+    log(data, indexP){
+        new Registration()._log(data, indexP)
+        return
     }
     
     register(indexP, rType){
         if((!indexP) && (!rType)){
             throw Error("Registration path and type are needed for registration with comet")
-        }else if(this.get_registration(indexP, rType)){
-            //return existing registration object
-            return new Registration.register(indexP, rType)
-        }else{
-            //register
-            return new Registration().register(indexP, rType);
+        }else if(!this.get_registration(indexP, rType)){
+            new Registration().register(indexP, rType);
         }
-    }
-    getLog(logP){
-        return new Registration().get_Registration(logP)
     }
 }
 
