189860d401f578d4442ed7aa157f1dbcbf2d6045
zeus <zeusofolympus1000@gmail.com>
2022-07-11 21:10:27 -0400

    even simpler

--- Comet/Comet.js
+++ Comet/Comet.js
@@ -3,8 +3,6 @@ import { Registry } from "./Source/Registry/Registry.js";
 
 export class Comet{
     constructor(root){
-        //Comet only needs to be initilized once for an entire project
-        //so long as the project name is provided
         this.root=root
         this.Registry = new Registry(this.root)
         this.index=process.cwd().split(this.root)[0]+this.root+"/Comet/index/";
@@ -27,13 +25,11 @@ export class Comet{
     }
 
     comet(...data){
-        //this should know the absolute path to the file from where comet() its called
         this._comet(data, this.getOriginP(new Error().stack.split('\n')))
     }
     _comet(data, origin){
-        //get Registration of the indexP, and use the Registration object to log to the log file
-        var registration = this.Registry.get_registration(origin, 'log');
-        registration.log(data)
+        new Registry().log(data, origin)
+
         //this stays here regardless 
         if (this.verbose){
             console.log(data.join(' '))
@@ -53,12 +49,3 @@ export class Comet{
 
 var comet = new Comet("MatchicSpells")
 comet.comet("some log")
-
-//Functions
-//register(path, type)  //registers a file with the registry and type
-//comet(...data) uses _comet(data, origin) to log to a registered file
-//gets the registered log file Registration if it exists, and log to it
-//  if it doesn't exist, register the log file and log to it
-
-//getCaller takes the creates a new Error and gets the stack then returns the file from where 
-//comet() was called back to comet() to properly find the Registration and log to it
--- Comet/Source/Registry/Registry.js
+++ Comet/Source/Registry/Registry.js
@@ -1,21 +1,3 @@
-//A Registry instance registers paths to the comet registry index
-//either way the path to the file must be created in the diff folder from
-//the project root
-
-//registry is just the existence of a file in this pattern
-//registry of a dir is the existence of a meta-file belonging to the dir
-//dirName.comet is how we will keep track of dir meta data
-//all diffs inside a file are kept in the diff file corresponding to
-//the actual registered file.
-
-
-//A diff hash chain is created in the dir meta file and dirs
-//higher in the tree hash these as well. 
-
-//This is not really intended to replace version control.
-//its designed to bring sanity to things you want to keep
-//an eye on. Like test cases. (things that shouldn't be removed unless its wholesale)
-
 import * as fs from "node:fs";
 
 export class Registration{
@@ -39,21 +21,28 @@ export class Registration{
 }
 
 export class Registry{
-    //TODO
+
+    //returns true or false if registration path and type exists
     exists(indexP, rType){
         return new Registration().exists(indexP, rType)
     }
-    log(data, indexP){
-        new Registration()._log(data, indexP)
-        return
+
+    log(data, origin){
+        new Registration()._log(data, this.registration(origin, 'log'))
     }
-    
+
+    //register if not registered
     register(indexP, rType){
         if((!indexP) && (!rType)){
             throw Error("Registration path and type are needed for registration with comet")
         }else if(!new Registration().get_registration(indexP, rType)){
             new Registration().register(indexP, rType);
         }
+    }
+    registration(origin, type){
+        //this gets the registration associated with 
+        //the file with the origin path
+
     }
 }
 
