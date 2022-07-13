25ddc442652b198b5f168767795dfdc981df091c
zeus <zeusofolympus1000@gmail.com>
2022-07-12 22:30:06 -0400

    SLOW DOWN! COME BACK TOMMOROW!

--- Spells.js
+++ Spells.js
@@ -7,9 +7,17 @@ import {Matchic} from "./Source/Matchic.js"
 
 class Spell{
     constructor(string){
-        this.state={}
-        this.string=string;
-        this.location=0;
+        //you can access the global state at anytime, but it won't be on the stack so that you can
+        //use the variables predicitably. You can have local variables in each iterative state
+        //that you can reaccess when it is popped off the stack.
+
+        //when you iterate over a string, it is either qualititative or quantititative
+        //When you change the quality of the next step, you should be able to get something
+        //specific and go back to the previous quality (like a look ahead) step a state variable
+        //from the lookahead
+        this.globalState = {string: string}
+        this.stateStack=[]
+        
         this.Matchic = new Matchic()
 
         Spell.prototype.nextLine= this.nextLine;
@@ -24,56 +32,87 @@ class Spell{
         Spell.prototype.up=this.up;
 
     }
+    _next(cb, currentState){
+        cb(this.globalState, this.stateStack)
+        this.stateStack.push(currentState)
+    }
 
     nextLine(cb){
         //separated by one newline
+        
+        this._next(cb, currentState)
         return this;
     }
 
     nextParagraph(cb){
         //separated by two newlines
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextSentance(cb){
         //separated by a period
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextInteger(cb){
+
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextFloat(cb){
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextScientific(cb){
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextOctet(cb){
+
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextHex(cb){
+
+        this._next(cb, currentState)
         return this;
     }
 
     nextBlock(cb){
+
+
+        this._next(cb, currentState)
         return this;
     }
 
-    up(cb){
-        //when you iterate over a string, it is either qualititative or quantititative
-        //When you change the quality of the next step, you should be able to get something
-        //specific and go back to the previous quality (like a look ahead) step a state variable
-        //from the lookahead
-        return this;
-    }
     nextChar(cb){
         return this;
     }
+
+    nextHTMLTag(){
+
+    }
+
+    up(cb){
+        //this just pops the currentState off of the stack 
+        //(it basically erases the previous nextSomething() 
+        //but allows you to keep a global variable)
+        this.stateStack.pop()
+        return this;
+    }
+
 
 }
 
