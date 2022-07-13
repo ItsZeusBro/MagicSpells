1b3c2392820b9e838ce43a0f27d9d9466f66a5c5
zeus <zeusofolympus1000@gmail.com>
2022-07-13 05:02:17 -0400

    a push pop variable works for some reason

--- Spells.js
+++ Spells.js
@@ -31,100 +31,106 @@ class Spell{
         Spell.prototype.iter=this.iter;
 
         this.globalState = {"subStr": string};
+        this.buffer=[];
         this.stateStack=[];
-        this.batch=[];
-        this.itr=0;
+        this.ugly_batch=[];
+        this.pushPop;
+        this.ugly_itr=0;
         this._next(()=>{}, this.globalState);
         this.Matchic = new Matchic();
 
     }
     _next(cb, currentState){
-        cb(currentState['match'], currentState, this.globalState)
-        if(this.itr){
-            this.itr--;
-            this.batch.push(currentState)
-            if (this.itr){
-                this.stateStack.push({"batch":this.batch, 'subStr':currentState['subStr']})
+        this.pushPop={"batch":this.ugly_batch, 'subStr':currentState['subStr']}
+        if(this.ugly_itr){
+            this.ugly_itr--;
+            this.ugly_batch.push(this.pushPop)
+            if (!this.ugly_itr){
+                this.stateStack.push({"batch":this.ugly_batch, 'subStr':currentState['subStr']})
+                this.ugly_itr=0;
+                this.ugly_batch=[];
                 console.log("STATE STACK====>", this.stateStack)
             }
         }else{
-            this.stateStack.push(currentState)
-            console.log("STATE STACK====>", this.stateStack)
+            if(cb){
+                cb(currentState['match'], currentState, this.globalState)
+            }
+            this.stateStack.push(this.pushPop)
         }
 
     }
 
     nextLine(cb){
         //separated by one newline
-        var match = new Matchic().nextLine(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextLine(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextParagraph(cb){
         //separated by two newlines
-        var match = new Matchic().nextParagraph(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextParagraph(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextSentance(cb){
         //separated by a period
-        var match = new Matchic().nextSentance(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextSentance(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextInteger(cb){
-        var match = new Matchic().nextInteger(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextInteger(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextFloat(cb){
-        var match = new Matchic().nextFloat(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextFloat(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextScientific(cb){
-        var match = new Matchic().nextScientific(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextScientific(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextOctet(cb){
-        var match = new Matchic().nextOctet(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextOctet(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextHex(cb){
-        var match = new Matchic().nextHex(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextHex(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextCodeBlock(cb, type){
-        var match = new Matchic().nextCodeBlock(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextCodeBlock(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
     nextFunction(cb, type){
-        var match = new Matchic().nextFunction(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextFunction(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextChar(cb){
-        var match = new Matchic().nextChar(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextChar(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
     nextHTML(){
-        var match = new Matchic().nextHTML(this.stateStack[this.stateStack.length-1]["subStr"])
-        this._next(cb, {"match":match, "subStr": this.stateStack[this.stateStack.length-1]["subStr"].replace(match, "")})
+        var match = new Matchic().nextHTML(this.pushPop["subStr"])
+        this._next(cb, {"match":match, "subStr": this.pushPop["subStr"].replace(match, "")})
         return this;
     }
 
@@ -142,7 +148,7 @@ class Spell{
         //fn is function name
         //cb is callback to pass to function name
         
-        this.itr=n;
+        this.ugly_itr=n;
         for(var i = 0; i<n; i++){
             if(fn=='nextLine'){this.nextLine(cb)}
             else if(fn=='nextParagraph'){this.nextParagraph(cb)}
@@ -155,10 +161,7 @@ class Spell{
             else if(fn=='nextCodeBlock'){this.nextCodeBlock(cb)}
             else if(fn=='nextFunction'){this.nextFunction(cb)}
             else if(fn=='nextHTML'){this.nextHTML(cb)}
-            this.itr--;
         }
-        this.batch=[];
-        this.itr=0;
         return this;
     }
 
@@ -167,5 +170,5 @@ class Spell{
 
 
 
-var results = new Spell(FLOAT_STR_CASE).iter(44, 'nextFloat', (match, cs, gs)=>{console.log(match)})
+var results = new Spell(FLOAT_STR_CASE).iter(2, 'nextFloat')
 
