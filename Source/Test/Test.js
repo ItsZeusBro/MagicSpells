7923863942b767da703e177666814831ada1c583
Zeus <zeusofolympus1000@gmail.com>
2022-07-14 23:45:18 -0400

    12 GB was not enough for the heap, we need cloud infraastructure for this test

--- Source/Test/Test.js
+++ Source/Test/Test.js
@@ -1,5 +1,6 @@
 import {Matchic} from "../Matchic.js"
 import {Spell} from "../../Spell.js"
+import { CHAR } from "../Spells/Spells.js"
 import * as util from "node:util"
 import * as assert from "node:assert"
 import {BOOKS} from "./Cases/Books/Books.js"
@@ -7,10 +8,11 @@ import {BOOKS} from "./Cases/Books/Books.js"
 class Test{
 	constructor(){
 		
-		this.Books()
+		//this.nextLine()
+		this.nextMatchicCHAR()
 	}
 
-	Books(){
+	nextLine(){
 		var opStack = new Spell(BOOKS, {'pageSize':50, 'pageOn': '\n'})
 		.iter('inf', 'nextLine', (match, cs, gs)=>{}).opStack
 		var bookStr=``
@@ -22,7 +24,23 @@ class Test{
 		})
 		assert.equal(bookStr.length, BOOKS.length)
 		assert.equal(bookStr, BOOKS)
-		console.log("PASSED")
+		console.log("nextLine PASSED")
+	}
+
+	nextMatchicCHAR(){
+		var opStack = new Spell(BOOKS, {'pageSize':50, 'pageOn': '\n'})
+		.iter('inf', 'nextMatchic', (match, cs, gs)=>{}, {'spells':[CHAR]}).opStack
+		var bookStr=``
+		opStack.forEach((element)=>{
+			if(element['match']){
+				bookStr+=element['match']
+				console.log(element)
+			}
+			//console.log(util.inspect(element, false, null, true))
+		})
+		assert.equal(bookStr.length, BOOKS.length)
+		assert.equal(bookStr, BOOKS)
+		console.log("nextLine PASSED")
 
 	}
 }
